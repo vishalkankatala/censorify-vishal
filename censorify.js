8ea401314115ea21a88b13e5df7f65b74ab2788b
Vishal Kankatala <vishal.kankatala@rackspace.com>
2014-12-20 17:52:18 -0600

    Second commit

---
+++ censorify.js
@@ -0,0 +1,27 @@
+var censored_words = ["sad","bad","mad"];
+var custom_censored_words = [];
+function censor(intStr) {
+ 	for (idx in censored_rods){
+		inStr = inStr.replace(censoredWords[idx], "****");
+		}
+	for(idx in customCensoredWords)	{
+		inStr = inStr.replace(customCensoredWords[idx], "****")
+		}
+		return inStr;}
+
+function addCensoredWord(word){
+	customCensoredWords.push(word);
+}
+
+function getCensoredWords(){
+	return censoredWords.concat(customCensoredWords);
+}
+export.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCesnoredWords = getCensoredWords;
+
+
+
+
+
+		
